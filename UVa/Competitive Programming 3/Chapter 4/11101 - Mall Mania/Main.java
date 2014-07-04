29dbd655d52458f7fdd491361b972f759fd6aa43
fire_bot <viadukm@gmail.com>
2014-07-04 10:12:54 +0100

    AC solution
    AC solution to 11101 - Mall Mania

--- UVa/Competitive Programming 3/Chapter 4/11101 - Mall Mania/Main.java
+++ UVa/Competitive Programming 3/Chapter 4/11101 - Mall Mania/Main.java
@@ -4,38 +4,24 @@ import java.io.*;
 
 public class Main
 {
-	public static int[][] city = int[2001][2001];
+	public static boolean[][] city = new boolean[2005][2005];
+	public static boolean[][] visited = new boolean[2005][2005];
 	
-	public static class Coord
+	public static Queue<Node> graph = new LinkedList<Node>();
+	
+	public static int dx[] = { 0, -1, 1, 0 };
+	public static int dy[] = { 1, 0, 0, -1 };
+	
+	public static class Node
 	{
-		public int x, y;
-		public Coord(int x, int y)
+		public int x, y, dist;
+		
+		public Node(int x, int y, int dist)
 		{
 			this.x = x;
 			this.y = y;
+			this.dist = dist;
 		}
-	}
-	
-	public static boolean readMall(BufferedReader reader, int mallNr)
-	{
-		int p = Integer.parseInt(line);
-				
-		if (p == 0)
-		{
-			return false;
-		}
-		
-		while(p != 0)
-		{
-			StringTokenizer token = new StringTokenizer(reader.readLine());
-			while(token.hasNextToken())
-			{
-				int a = Integer.parseInt(token.nextToken());
-				int s = Integer.parseInt(token.nextToken());
-				city[a][s] = mallNr;
-			}
-		}
-		return true;
 	}
 	
 	public static void main(String[] args) throws IOException
@@ -45,8 +31,76 @@ public class Main
 			
 			while((line = reader.readLine()) != null)
 			{
-				readMall(reader, 1);
-				readMall(reader, 2);
+				int p = Integer.parseInt(line);
+				
+				if (p == 0)
+				{
+					break;
+				}
+				for(int i = 0; i < 2005; ++i)
+				{
+					for(int j = 0; j < 2005; ++j)
+					{
+						visited[i][j] = city[i][j] = false;
+					}
+				}
+				
+				graph.clear();
+				
+				while(p != 0)
+                {
+                    StringTokenizer token = new StringTokenizer(reader.readLine());
+                    while(token.hasMoreTokens())
+                    {
+                        int a = Integer.parseInt(token.nextToken());
+                        int s = Integer.parseInt(token.nextToken());
+                        graph.add(new Node(a, s, 0));
+                        p--;
+                    }
+                }
+                
+                p = Integer.parseInt(reader.readLine());
+                
+                while(p != 0)
+                {
+                    StringTokenizer token = new StringTokenizer(reader.readLine());
+                    while(token.hasMoreTokens())
+                    {
+                        int a = Integer.parseInt(token.nextToken());
+                        int s = Integer.parseInt(token.nextToken());
+                        city[a][s] = true;
+                        p--;
+                    }
+                }
+
+                System.out.println(bfs());
 			}
+	}
+	
+	public static int bfs()
+	{
+		while(!graph.isEmpty())
+		{
+			Node node = graph.poll();
+			for(int i = 0; i < 4; i++)
+			{
+				int x = node.x + dx[i];
+				int y = node.y + dy[i];
+				
+				if (x >= 0 && x < 2000 && y >= 0 && y < 2000)
+				{
+					if (!visited[x][y])
+					{
+						visited[x][y] = true;
+						if (city[x][y])
+						{
+							return node.dist + 1;
+						}
+						graph.add(new Node(x, y, node.dist + 1));
+					}
+				}
+			}
+		}
+		return -1;
 	}
 }
