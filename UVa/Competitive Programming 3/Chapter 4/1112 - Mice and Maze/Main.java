77398d073b44fca5d9067e337919eb7c0485fe4b
fire_bot <viadukm@gmail.com>
2014-07-08 11:28:04 +0100

    Implementation with Dijkstra
    Inverted paths and calculating Dijkstra from end.. Somehow still WA...

--- UVa/Competitive Programming 3/Chapter 4/1112 - Mice and Maze/Main.java
+++ UVa/Competitive Programming 3/Chapter 4/1112 - Mice and Maze/Main.java
@@ -4,100 +4,106 @@ import java.io.*;
 
 public class Main
 {
-	//public static int[][] maze = new int[100][100];
-	public static Map<Integer, List<Integer>> maze = new HashMap<Integer, List<Integer>>();
-	
-	public static Map<Integer, Vertex> map = new HashMap<Integer, Vertex>();
-	
-	public static PriorityQueue<Vertex> pq = new PriorityQueue<Vertex>();
-	public static List<Vertex> list = new ArrayList<Vertex>();
-	
-	public static class Vertex extends Comparable<Vertex>
-	{
-		public int id;
-		public int distance;
-		
-		public Vertex(int id, distance)
-		{
-			this.id = id;
-			this.distance = distance;
-		}
-		
-		public int compareTo(Vertex vertex)
-		{
-			return distance - vertex.distance;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException
-	{
+    public static Map<Integer, List<Integer>> maze = new HashMap<Integer, List<Integer>>();
+    public static Map<Integer, Vertex> map = new HashMap<Integer, Vertex>();
+    public static PriorityQueue<Vertex> pq = new PriorityQueue<Vertex>();
+    public static int[][] cost = new int[101][101];
+    
+    public static class Vertex implements Comparable<Vertex>
+    {
+        public int id;
+        public int distance;
+
+        public Vertex(int id, int distance)
+        {
+            this.id = id;
+            this.distance = distance;
+        }
+
+        public int compareTo(Vertex vertex)
+        {
+            return distance - vertex.distance;
+        }
+    }
+
+    public static void main(String[] args) throws IOException
+    {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		int cases = Integer.parseInt(reader.readLine());
-		
-		while(cases-- != 0)
-		{
-			reader.readLine();
-			int N = Integer.parseInt(reader.readLine());
-			int E = Integer.parseInt(reader.readLine());
-			int T = Integer.parseInt(reader.readLine());
-			int M = Integer.parseInt(reader.readLine());
-			
-			for(int i = 1; i <= M; ++i)
-			{
-				StringTokenizer token = new StringTokenizer(reader.readLine());
-				int a = Integer.parseInt(token.nextToken());
-				int b = Integer.parseInt(token.nextToken());
-				int cost = Integer.parseInt(token.nextToken());
-				
-				List<Integer> adjList = maze.get(a);
-				
-				if (adjList == null)
-				{
-					adjList = new ArrayList<Integer>();
-				}
-				
-				adjList.add(b);
-				adjList.add(cost);
-				Vertex vertex = new Vertex(a, Integer.MAX_VALUE);
-				list.add(vertex);
-				map.put(a, vertex);
-			}
-			
-			int result = 0;
-			for(Vertex vertex : list)
-			{
-				vertex.distance = 0;
-				pq.clear();
-				pq.addAll(list);
-				
-				while(!pq.isEmpty())
-				{
-					Vertex u = pq.poll();
-					
-					List<Integer> adjList = maze.get(u);
-					
-					if (u == E)
-					{
-						result += u.distance;
-						break;
-					}
-					
-					for(int i = 0; i < adjList.size(); i += 2)
-					{
-						int vi = adjList.get(i);
-						int cost = adjList.get(i + 1);
-						int alt = u.distance + cost;
-						Vertex v = map.get(vi);
-						
-						if (alt < v.distance)
-						{
-							pq.remove(v);
-							v.distance = alt;
-							pq.offer(v);
-						}
-					}
-				}
-			}
-		}
-	}
+        int cases = Integer.parseInt(reader.readLine());
+
+        for(int z = 0; z < cases; ++z)
+        {
+            reader.readLine();
+            int N = Integer.parseInt(reader.readLine());
+            int E = Integer.parseInt(reader.readLine());
+            int T = Integer.parseInt(reader.readLine());
+            int M = Integer.parseInt(reader.readLine());
+
+            if (z != 0)
+            {
+                System.out.println();
+            }
+            
+            maze.clear();
+            map.clear();
+            pq.clear();
+            
+            for(int i = 1; i <= N; ++i)
+            {
+                int dist = Integer.MAX_VALUE;
+                
+                if (i == E)
+                {
+                   dist = 0;
+                }
+                
+                maze.put(i, new ArrayList<Integer>());
+                Vertex vertex = new Vertex(i, dist);
+                map.put(i, vertex);
+                pq.add(vertex);
+            }
+            
+            for(int i = 1; i <= M; ++i)
+            {
+                StringTokenizer token = new StringTokenizer(reader.readLine());
+                int a = Integer.parseInt(token.nextToken());
+                int b = Integer.parseInt(token.nextToken());
+                int c = Integer.parseInt(token.nextToken());
+
+                maze.get(b).add(a);
+                cost[b][a] = c;
+            }
+            
+            while(!pq.isEmpty())
+            {
+                Vertex u = pq.poll();
+
+                for(Integer vi : maze.get(u.id))
+                {                    
+                    Vertex v = map.get(vi);
+                    
+                    if (u.distance + cost[u.id][vi] < v.distance)
+                    {
+                        pq.remove(v);
+                        v.distance = u.distance + cost[u.id][vi];
+                        pq.offer(v);
+                    }
+                }
+            }
+            
+            int result = 0;
+            
+            for(int i = 1; i <= N; ++i)
+            {
+                Vertex v = map.get(i);
+                
+                if (T >= v.distance)
+                {
+                    result++;
+                }
+            }
+            
+            System.out.println(result);
+        }
+    }
 }
