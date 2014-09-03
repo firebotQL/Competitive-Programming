526d2df8539a3da25061fc40dc504de75ea7c2a5
firebotQL <viadukm@gmail.com>
2014-09-03 20:56:00 +0100

    104 - Arbitrage (AC)
    Solution for Arbitrage

---
+++ UVa/Competitive Programming 3/Chapter 4/104 - Arbitrage/Main.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) throws IOException
+	{
+		Scanner scanner = new Scanner(System.in));
+		while(scanner.has
+	}
+}
