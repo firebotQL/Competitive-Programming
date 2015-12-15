565f0c45a1ddfb68e146ef970c575ca512e2a06f
firebotQL <viadukm@gmail.com>
2015-12-15 22:21:45 +0000

    Chapter 3 - Sort Stack
    Chapter 3 - Sort Stack

---
+++ CTCI/Chapter 3/p5/Main.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+/**
+* Write a programm to sort a stack such taht the smallest items are on the top. You can use
+* an additional temporary stack, but you may not copy the elements into any other data structure
+* (such as an array). The stack supports the following operations: push, pop, peek, and isEmpty. 
+*/
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(5);
+        stack.push(3);
+        stack.push(2);
+        Stack<Integer> tmpStack = new Stack<Integer>();
+        while(!stack.isEmpty()) {
+            Integer element = stack.pop();
+            while(!tmpStack.isEmpty() && tmpStack.peek() < element) {
+                stack.push(tmpStack.pop());
+            }
+            tmpStack.push(element);
+        }
+
+        Stack<Integer> swap = stack;
+        stack = tmpStack;
+        tmpStack = swap;
+
+        while(!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
