c875089fd6bf9361bb1e8b46e25a9b4710020dfc
firebotQL <viadukm@gmail.com>
2016-01-29 18:43:32 +0000

    Chapter 4 - BST Sequence

--- CTCI/Chapter 4/p9/Main.java
+++ CTCI/Chapter 4/p9/Main.java
@@ -1,6 +1,4 @@
-package chapter4.p9;
-
-import java.util.ArrayList;
+import java.util.*;
 
 /**
  * BST Sequences: A binary search tree was created by traversing through an array from left to right and inserting each
@@ -15,19 +13,89 @@ public class Main {
             this.key = key;
         }
     }
-    public static void main(String[] args) {
-        Node root = new Node(2);
-        root.left = new Node(1);
-        root.right = new Node(3);
-        printTree(root);
+
+    public static List<List<Integer>> bfs(Node node) {
+        Queue<Node> queue1 = new LinkedList();
+        Queue<Node> queue2 = new LinkedList();
+        queue1.add(node);
+        List<List<Integer>> results = new ArrayList();
+
+        while(!queue1.isEmpty()) {
+            List<Integer> level = new ArrayList();
+            while(!queue1.isEmpty()) {
+                Node current = queue1.poll();
+                level.add(current.key);
+
+                if (current.left != null) {
+                    queue2.add(current.left);
+                }
+
+                if (current.right != null) {
+                    queue2.add(current.right);
+                }
+            }
+
+            Queue<Node> tmp = queue1;
+            queue1 = queue2;
+            queue2 = tmp;
+
+            List<List<Integer>> tmpResults = new ArrayList();
+            permute(level, 0, tmpResults);
+            results = getNewResults(results, tmpResults);
+        }
+        return results;
     }
 
-    private static ArrayList<Node> printTree(Node node) {
-        if (node == null) {
-            return new ArrayList<Node>();
+    private static List<List<Integer>> getNewResults(List<List<Integer>> results, List<List<Integer>> tmpResults) {
+        List<List<Integer>> newResults = new ArrayList();
+        if (!results.isEmpty()) {
+            for(List<Integer> tmpResult : tmpResults) {
+                for(List<Integer> result : results) {
+                    List<Integer> subResult = new ArrayList(result);
+                    subResult.addAll(tmpResult);
+                    newResults.add(subResult);
+                }
+            }
+        } else {
+            newResults = tmpResults;
         }
-        System.out.print(node.key + " ");
-        ArrayList<Node> left = printTree(node.left);
-        ArrayList<Node> right = printTree(node.right);
+        return newResults;
+    }
+
+    public static void permute(List<Integer> array, int k, List<List<Integer>> results) {
+        for (int i = k; i < array.size(); i++) {
+            Collections.swap(array, i, k);
+            permute(array, k + 1, results);
+            Collections.swap(array, k, i);
+        }
+
+        if (k == array.size() - 1) {
+            results.add(new ArrayList<>(array));
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(5);
+        root.left = new Node(3);
+        root.right = new Node(7);
+        root.left.left = new Node(2);
+        root.left.right = new Node(4);
+        root.right.left = new Node(6);
+        root.right.right = new Node(8);
+
+        Node root1 = new Node(3);
+        root1.left = new Node(1);
+        root1.right = new Node(2);
+
+        for(List<Integer> result : bfs(root)) {
+            System.out.println(Arrays.toString(result.toArray()));
+        }
+
+        System.out.println();
+
+        for(List<Integer> result : bfs(root1)) {
+            System.out.println(Arrays.toString(result.toArray()));
+        }
     }
 }
+
