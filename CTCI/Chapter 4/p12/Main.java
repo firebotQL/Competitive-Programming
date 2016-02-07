15ddd4f23f7c49fa0b829faf532234bb18079eac
firebotQL <viadukm@gmail.com>
2016-02-07 21:47:51 +0000

    Chapter 4 - Paths with Sum

--- CTCI/Chapter 4/p12/Main.java
+++ CTCI/Chapter 4/p12/Main.java
@@ -1,33 +1,64 @@
+import java.util.*;
+
 /**
  * Paths with Sum: You are given a binary tree in which each node contains an integer value (which might be positive or
  * negative). Design an algorithm to count the number of paths that sum to a given value. The path does not need to
  * start or end at the root or a leaf, but it must go downwards (travelling only from parent nodes to child nodes).
  */
- 
-public class Main {
-    public static int counter = 0;
 
-    public static void main(String[] args) {
-        int sum = 6;
-        int[] abc = { 1, 3, 2, 4, -6, 2 };
-        countSums(abc, sum);
-        System.out.println(counter);
+public class Main {
+    public static class Node {
+        int key;
+        Node left, right;
+        public Node(int key) {
+            this.key = key;
+        }
     }
 
-    private static void countSums(int[] arr, int sum) {
-        for(int i = 0; i < arr.length; i++) {
-            int newSum = arr[i];
-            for(int j = i + 1; j <= arr.length; j++) {
-                if (newSum == sum) {
-                    counter++;
-                    System.out.println("i = " + i + " j = " + (j-1) + " : " + newSum);
-                }
-                if (j == arr.length) {
-                    break;
-                }
-                newSum += arr[j];
-            }
-        }
+    public static void main(String[] args) {
+        Node root = new Node(10);
+        root.left = new Node(5);
+        root.right = new Node(-3);
+        root.left.left = new Node(3);
+        root.left.right = new Node(1);
+        root.left.left.left = new Node(3);
+        root.left.left.right = new Node(-2);
+        root.left.right.right = new Node(2);
+        root.right.right = new Node(11);
 
+        int result = countPathsWithSum(root, 8);
+        System.out.println("Number of paths sum up to 8 equal to " + result);
+    }
+
+    public static int countPathsWithSum(Node current, int sum) {
+        if (current == null) return 0;
+        Map<Integer, Integer> pathCount = new HashMap<>();
+        updateHashTable(pathCount, 0, 1);
+        return countPathsWithSum(current, sum, 0, pathCount);
+    }
+
+    private static int countPathsWithSum(Node current, int sum, int currentSum, Map<Integer, Integer> pathCount) {
+        if (current == null) return 0;
+
+        currentSum += current.key;
+        updateHashTable(pathCount, currentSum, 1);
+        int newSum = currentSum - sum;
+        int totalPaths = pathCount.containsKey(newSum) ? pathCount.get(newSum) : 0;
+
+        totalPaths += countPathsWithSum(current.left, sum, currentSum, pathCount);
+        totalPaths += countPathsWithSum(current.right, sum, currentSum, pathCount);
+
+        updateHashTable(pathCount, currentSum, -1);
+
+        return  totalPaths;
+    }
+
+
+    public static void updateHashTable(Map<Integer, Integer> hashTable, int key, int delta) {
+        Integer value = hashTable.get(key);
+        if (value == null) {
+            value = 0;
+        }
+        hashTable.put(key, value + delta);
     }
 }
