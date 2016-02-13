9ed8e804b75d631200b72ee335afc18e73824bb1
firebotQL <viadukm@gmail.com>
2016-02-13 23:47:33 +0000

    Chapter 5 - Next Number
    Template with problem statement. No solution yet.

---
+++ CTCI/Chapter 5/p4/Main.java
@@ -0,0 +1,11 @@
+package chapter5.p4;
+
+/**
+ * Next Number: Given a positive integer, print the next smallest and the next largest number that have the same
+ * number of 1 bits in their binary representation.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
