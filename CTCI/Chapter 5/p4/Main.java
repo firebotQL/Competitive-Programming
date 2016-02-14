996099d190f8306b9bc574c3ed51b4aa1fd6316e
firebotQL <viadukm@gmail.com>
2016-02-14 22:57:42 +0000

    Removed package of file
    Removed package of file

--- CTCI/Chapter 5/p4/Main.java
+++ CTCI/Chapter 5/p4/Main.java
@@ -1,5 +1,3 @@
-package chapter5.p4;
-
 /**
  * Next Number: Given a positive integer, print the next smallest and the next largest number that have the same
  * number of 1 bits in their binary representation.
