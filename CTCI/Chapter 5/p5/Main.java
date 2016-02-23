a39848f21072127e1cf159dbc4cdac06606dcca8
firebotQL <viadukm@gmail.com>
2016-02-23 21:44:12 +0000

    Chapter 5 - Debugger
    Chapter 5 - Debugger

---
+++ CTCI/Chapter 5/p5/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Debugger: Explain what the following code does: ((n & (n - 1)) == 0)
+ */
+public class Main {
+    public void main(String[] args) {
+        // Assumption it is binary arithmetic
+        // Lets take an example number 11 in binary
+        // 1.   n - 1 should return 10 because 10 + 1 in binary equals 11
+        // 2.   n & 11 should be equal to 10
+        // 3.   10 == 0 equals false
+    }
+}
