b86e10141082bb33f60aca079a807311aff61a9f
Viaceslavas Duk <viadukm@gmail.com>
2016-08-22 23:04:34 +0100

    Divisible Sum Pairs
    Divisible Sum Pairs

---
+++ HackerRank/Algorithms/Implementation/Divisible-Sum-Pairs/Solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                if ((a[i] + a[j]) % k == 0) {
+                    cnt++;
+                }   
+            }
+        }
+        System.out.println(cnt);
+    }
+}
