2d4c7ca1a0eaf500bb0249b9aca106e4aa3dd441
firebotQL <viadukm@gmail.com>
2017-03-23 19:04:23 +0000

    11475 - Extend to Palindrome (Solution)

--- UVa/Competitive Programming 3/Chapter 6/11475 - Extend to Palindromes/Main.java
+++ UVa/Competitive Programming 3/Chapter 6/11475 - Extend to Palindromes/Main.java
@@ -2,40 +2,60 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-  public static StringBuilder sb = new StringBuilder();
   public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        while(sc.hasNext()) {
            String line = sc.next();
-           sb.setLength(0);
-           sb.append(line);
-           int palindromeStart = 0;
-           int i = 0, j = line.length() - 1;
-           while(i < j) {
-               if (equalCharacters(line, i, j)) {
-                   j--; i++;
-               } else {
-                   j = line.length() - 1;
-                   if (palindromeStart == i) {
-                       i++;
-                   } else {
-                       i = palindromeStart + 1;
-                   }
-                   palindromeStart = i;
-               }
+           if (line.length() == 1) {
+             System.out.println(line);
+             continue;
            }
-           if (i >= j) {
-               while(--palindromeStart >= 0) {
-                  sb.append(line.charAt(palindromeStart));
-               }
-           } else {
-               sb.append(line);
-           }
-           System.out.println(sb.toString());
+           String rev = new StringBuilder(line).reverse().toString();
+
+           int[] T = generatePrefixArray(rev);
+           int startIdx = match(line, rev, T);
+
+           System.out.println(line + rev.substring(startIdx, rev.length()));
        }
   }
 
-  private static boolean equalCharacters(String line, int i, int j) {
-      return line.charAt(i) == line.charAt(j);
+  private static int match(String text, String pattern, int[] T) {
+          int j = 0, i = 0;
+          while(j + i < text.length()) {
+              if (pattern.charAt(i) == text.charAt(i + j)) {
+                  if (j == pattern.length()) {
+                      return i;
+                  }
+                  i++;
+              } else {
+                  j += i - T[i];
+                  if (T[i] > -1) {
+                      i = T[i];
+                  } else {
+                      i = 0;
+                  }
+              }
+          }
+          return i;
+      }
+
+  private static int[] generatePrefixArray(String pattern) {
+      int i = 2, sub = 0;
+      int[] T = new int[pattern.length()];
+      T[0] = -1;
+      T[1] = 0;
+      while(i < pattern.length()) {
+          if (pattern.charAt(i-1) == pattern.charAt(sub)) {
+              sub++;
+              T[i] = sub;
+              i++;
+          } else if (sub > 0) {
+              sub = T[sub];
+          } else {
+              T[i] = 0;
+              i++;
+          }
+      }
+      return T;
   }
 }
