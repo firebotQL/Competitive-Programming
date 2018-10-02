46f5927d23d854d82fbbbeb788238b4733544573
firebotQL <viadukm@gmail.com>
2018-10-02 20:18:58 +0100

    Leetcode - Rotate Array
    Simple rotation :)

---
+++ Leetcode/Top Interview Questions/Easy Collection/Array/Rotate Array/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        rotate(nums, 0, nums.length - 1);
+        rotate(nums, 0, k - 1);
+        rotate(nums, k, nums.length - 1);
+    }
+
+    public void rotate(int[] nums, int start, int end) {
+        while(start < end) {
+            int tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            end--;
+            start++;
+        }
+    }
+}
