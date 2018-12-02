894c3823f7fbfcbdc3d6e2be791e2f3e3b9f2f1a
firebotQL <viadukm@gmail.com>
2018-12-02 09:52:31 +0000

    Day 1 Part Two

--- Advent of Code/2018/Day 1/Part Two/Main.java
+++ Advent of Code/2018/Day 1/Part Two/Main.java
@@ -1,8 +1,17 @@
 import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.concurrent.atomic.AtomicLong;
 
-public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println(br.lines().mapToInt(Integer::valueOf).sum());
-  }
+class Main {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Long> values = br.lines().map(Long::valueOf).collect(Collectors.toList());
+        AtomicLong frequency = new AtomicLong(0);
+        Set<Long> frequencies = new HashSet<>(){{ add(0l); }};
+        IntStream.iterate(0, n -> frequencies.add(frequency.addAndGet(values.get(n))), n -> (n + 1) % values.size()).count();
+        System.out.println(frequency);
+    }
+
 }
