5b65213d3e351e44f307620c9773f8798f0bf3b5
Viaceslavas Duk <viadukm@gmail.com>
2020-01-23 21:56:51 +0000

    Initial implementation

---
+++ Codewars/Expression Matter/main.ts
@@ -0,0 +1,9 @@
+export function expressionsMatter(a: number, b: number, c: number): number {
+   
+   const exp1 =  a * (b + c);
+   const exp2 = a * b * c;
+   const exp3 = (a + b) * c;
+   const exp4 = a + b + c;
+   
+   return Math.max(one, two, three, four);
+}
