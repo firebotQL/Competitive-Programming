6eac24744aff33294cf667326625e7cf4ad40e94
Viaceslavas Duk <viadukm@gmail.com>
2020-02-13 20:59:54 +0000

    Solution implementation

--- Codewars/Palindrome Strings/main.ts
+++ Codewars/Palindrome Strings/main.ts
@@ -1,3 +1,3 @@
-export function parseF(s:string):number {
-  return isNaN(Number(s)) ? null : Number(s);
+export function isPalindrome(line: string): boolean {
+  return line.split('').every((el, idx) => el === line.charAt(line.length - idx - 1));
 }
