b079f945bb6318b2f3faa99fc969a4f9d407c2f0
Viaceslavas Duk <viadukm@gmail.com>
2020-02-19 19:25:32 +0000

    Update main.ts

--- Codewars/8 kyu/Beginner Series #2 Clock/main.ts
+++ Codewars/8 kyu/Beginner Series #2 Clock/main.ts
@@ -1,1 +1,3 @@
-export function past(h: number, m: number, s: number) => h * 3600 * 1000 + m * 60 * 1000 + s * 1000;
+export function past(h: number, m: number, s: number): number {
+  return ((h * 60 + m) * 60  + s) * 1000;
+}
