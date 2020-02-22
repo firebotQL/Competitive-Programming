3f468add602db3014cc0d1603ecd278e28121310
Viaceslavas Duk <viadukm@gmail.com>
2020-02-22 00:11:55 +0000

    Updated version

--- Codewars/7 kyu/The Baum-Sweet sequence/main.ts
+++ Codewars/7 kyu/The Baum-Sweet sequence/main.ts
@@ -1,11 +1,7 @@
 export function* baumSweet(): Generator {
-  let num: number = 0;
+  yield 1;
+  let num: number = 1;
   while(true) {
-    const result = num.toString(2).split('').every((el: string, idx: number, arr: string[]) => {
-        return idx + 1 === arr.length || !(el === '0' && el === arr[idx + 1]);
-    }) ? 1 : 0;
-    console.log(`Value: ${num} Binary: ${num.toString(2)} Consecutive: ${result}`);
-    num++;
-    yield result;
+    yield +(num++).toString(2).split('1').every((el) => el.length % 2 === 0);
   }
 }
