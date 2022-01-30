a4e7901e1b60719e8e081f46ab47eff2bc756616
firebotQL <viadukm@gmail.com>
2022-01-30 01:21:23 +0000

    Update main.js
    Updated version to avoid duplicate quadruplets

--- Educative/Grokking The Coding Interview/Two Pointers/Quadruple Sum to Target (medium)/main.js
+++ Educative/Grokking The Coding Interview/Two Pointers/Quadruple Sum to Target (medium)/main.js
@@ -3,8 +3,14 @@ const search_quadruplets = function(arr, target) {
   // x + y + z + t = target
   arr.sort((a,b) => a-b);
   for(let i = 0; i < arr.length - 3; i++) {
+    if (i > 0 && arr[i] === arr[i - 1]) {
+      continue;
+    }
     const x = arr[i];
     for (let j = i + 1; j < arr.length - 2; j++) {
+      if (j > i + 1 && arr[j] === arr[j - 1]) {
+        continue;
+      }
       const y = arr[j];
        let left = j + 1;
        let right = arr.length - 1;
@@ -13,7 +19,8 @@ const search_quadruplets = function(arr, target) {
          const t = arr[right];
          if (x + y + z + t === target) {
            quadruplets.push([x, y, z, t]);
-           break;
+           left++;
+           right--;
          } else if (x + y + z + t < target) {
            left++;
          } else {
