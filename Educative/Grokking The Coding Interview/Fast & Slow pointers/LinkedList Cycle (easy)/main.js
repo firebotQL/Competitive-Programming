308e32e4115ee5bdea4ff40856d3d9d9a9704247
firebotQL <viadukm@gmail.com>
2022-01-31 23:44:19 +0000

    Create main.js

---
+++ Educative/Grokking The Coding Interview/Fast & Slow pointers/LinkedList Cycle (easy)/main.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value, next=null){
+    this.value = value;
+    this.next = next;
+  }
+}
+
+const has_cycle = function(head) {
+  let slow = head;
+  let fast = head;
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false
+}
+
+
+head = new Node(1)
+head.next = new Node(2)
+head.next.next = new Node(3)
+head.next.next.next = new Node(4)
+head.next.next.next.next = new Node(5)
+head.next.next.next.next.next = new Node(6)
+console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+
+head.next.next.next.next.next.next = head.next.next
+console.log(`LinkedList has cycle: ${has_cycle(head)}`)
+
+head.next.next.next.next.next.next = head.next.next.next
+console.log(`LinkedList has cycle: ${has_cycle(head)}`)
