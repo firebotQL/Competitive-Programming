f90a3dba79f5c4f564d6b27a78a17fde3559a5e6
firebotQL <viadukm@gmail.com>
2022-07-31 16:43:38 +0100

    Added implementation of Reverse a LinkedList (easy)

---
+++ Educative/Grokking The Coding Interview/Reverse a LinkedList (easy)/main.js
@@ -0,0 +1,36 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+
+  get_list() {
+    result = "";
+    temp = this;
+    while (temp !== null) {
+      result += temp.value + " ";
+      temp = temp.next;
+    }
+    return result;
+  }
+}
+
+const reverse = function (head) {
+  let previous = null;
+  while (head != null) {
+    const tmpPointer = head.next;
+    head.next = previous;
+    previous = head;
+    head = tmpPointer;
+  }
+  return previous;
+};
+
+head = new Node(2);
+head.next = new Node(4);
+head.next.next = new Node(6);
+head.next.next.next = new Node(8);
+head.next.next.next.next = new Node(10);
+
+console.log(`Nodes of original LinkedList are: ${head.get_list()}`);
+console.log(`Nodes of reversed LinkedList are: ${reverse(head).get_list()}`);
