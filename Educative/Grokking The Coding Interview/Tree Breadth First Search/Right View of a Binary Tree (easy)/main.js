104faa6262a17d70bd5efba6bd0c65f7805a2cbf
firebotQL <viadukm@gmail.com>
2022-08-01 22:57:15 +0100

    Added Right View of a Binary Tree (easy) implementation

--- Educative/Grokking The Coding Interview/Tree Breadth First Search/Right View of a Binary Tree (easy)/main.js
+++ Educative/Grokking The Coding Interview/Tree Breadth First Search/Right View of a Binary Tree (easy)/main.js
@@ -6,34 +6,23 @@ class TreeNode {
     this.left = null;
     this.right = null;
   }
-
-  // tree traversal using 'next' pointer
-  print_tree() {
-    result = "Traversal using 'next' pointer: ";
-    current = this;
-    while (current != null) {
-      result += current.value + " ";
-      current = current.next;
-    }
-    console.log(result);
-  }
 }
 
-const connect_all_siblings = function (root) {
-  if (root === null) return root;
+const tree_right_view = function (root) {
+  result = [];
+  if (root === null) return result;
   const deque = new Deque();
   deque.push(root);
-  let previous;
   while (deque.length > 0) {
     let currentLevelSize = deque.length;
     while (currentLevelSize-- != 0) {
       const node = deque.shift();
-      if (previous) previous.next = node;
-      previous = node;
+      if (currentLevelSize === 0) result.push(node.value);
       if (node.left) deque.push(node.left);
       if (node.right) deque.push(node.right);
     }
   }
+  return result;
 };
 
 var root = new TreeNode(12);
@@ -42,5 +31,5 @@ root.right = new TreeNode(1);
 root.left.left = new TreeNode(9);
 root.right.left = new TreeNode(10);
 root.right.right = new TreeNode(5);
-connect_all_siblings(root);
-root.print_tree();
+root.left.left.left = new TreeNode(3);
+console.log("Tree right view: " + tree_right_view(root));
