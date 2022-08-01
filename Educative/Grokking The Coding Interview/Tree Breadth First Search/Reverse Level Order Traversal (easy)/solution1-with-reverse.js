52cc162cfe337067053b43fe20581febc4b75964
firebotQL <viadukm@gmail.com>
2022-08-01 20:06:22 +0100

    Added Reverse Level Order Traversal (easy) two solutions

--- Educative/Grokking The Coding Interview/Tree Breadth First Search/Reverse Level Order Traversal (easy)/solution1-with-reverse.js
+++ Educative/Grokking The Coding Interview/Tree Breadth First Search/Reverse Level Order Traversal (easy)/solution1-with-reverse.js
@@ -1,28 +1,32 @@
 const Deque = require("./collections/deque"); //http://www.collectionsjs.com
 
 class TreeNode {
-  constructor(val) {
-    this.val = val;
+  constructor(value) {
+    this.value = value;
     this.left = null;
     this.right = null;
   }
 }
 
 const traverse = function (root) {
+  if (!root) return [];
+
   result = [];
-  var deque = new Deque();
+  const deque = new Deque();
   deque.push(root);
   while (deque.length > 0) {
     const level = [];
-    const currentLength = deque.length;
-    for (let i = 0; i < currentLength; i++) {
+    let currentSize = deque.length;
+    while (currentSize-- !== 0) {
       const node = deque.shift();
-      level.push(node.val);
+      level.push(node.value);
       if (node.left) deque.push(node.left);
       if (node.right) deque.push(node.right);
     }
-    result.push([level]);
+    result.push(level);
   }
+
+  result.reverse();
   return result;
 };
 
@@ -32,4 +36,4 @@ root.right = new TreeNode(1);
 root.left.left = new TreeNode(9);
 root.right.left = new TreeNode(10);
 root.right.right = new TreeNode(5);
-console.log(`Level order traversal: ${traverse(root)}`);
+console.log(`Reverse level order traversal: ${traverse(root)}`);
--- Educative/Grokking The Coding Interview/Tree Breadth First Search/Reverse Level Order Traversal (easy)/solution2-without-reverse.js
+++ Educative/Grokking The Coding Interview/Tree Breadth First Search/Reverse Level Order Traversal (easy)/solution2-without-reverse.js
@@ -1,28 +1,30 @@
 const Deque = require("./collections/deque"); //http://www.collectionsjs.com
 
 class TreeNode {
-  constructor(val) {
-    this.val = val;
+  constructor(value) {
+    this.value = value;
     this.left = null;
     this.right = null;
   }
 }
 
 const traverse = function (root) {
-  result = [];
-  var deque = new Deque();
+  const result = new Deque();
+  if (!root) return result;
+  const deque = new Deque();
   deque.push(root);
   while (deque.length > 0) {
     const level = [];
-    const currentLength = deque.length;
-    for (let i = 0; i < currentLength; i++) {
+    let currentSize = deque.length;
+    while (currentSize-- !== 0) {
       const node = deque.shift();
-      level.push(node.val);
+      level.push(node.value);
       if (node.left) deque.push(node.left);
       if (node.right) deque.push(node.right);
     }
-    result.push([level]);
+    result.unshift(level);
   }
+
   return result;
 };
 
@@ -32,4 +34,4 @@ root.right = new TreeNode(1);
 root.left.left = new TreeNode(9);
 root.right.left = new TreeNode(10);
 root.right.right = new TreeNode(5);
-console.log(`Level order traversal: ${traverse(root)}`);
+console.log(`Reverse level order traversal: ${traverse(root).toArray()}`);
