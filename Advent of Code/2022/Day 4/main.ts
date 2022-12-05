8bee5a399ac96e451b45e22cc34d229e021d62c3
firebotQL <viadukm@gmail.com>
2022-12-05 09:53:04 +0000

    Solution for Day 4 Advent of Code

---
+++ Advent of Code/2022/Day 4/main.ts
@@ -0,0 +1,19 @@
+import * as fs from "fs";
+const file = fs.readFileSync("input.txt", "utf8");
+
+let result = 0;
+
+file.split(/\r?\n/).forEach((line: string) => {
+  const [elf1, elf2] = line.split(",");
+  const [elf1Start, elf1End] = elf1.split("-").map((x) => parseInt(x));
+  const [elf2Start, elf2End] = elf2.split("-").map((x) => parseInt(x));
+
+  if (
+    (elf1Start <= elf2Start && elf2End <= elf1End) ||
+    (elf2Start <= elf1Start && elf1End <= elf2End)
+  ) {
+    result++;
+  }
+});
+
+console.log(result);
